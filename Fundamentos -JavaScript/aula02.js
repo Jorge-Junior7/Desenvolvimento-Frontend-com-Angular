8dbd6e14a767bd7e5c31fc0f91acaa2014f2235d
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-19 15:43:23 -0300

    aula 02.1 IF ELSE(nº par)

--- Fundamentos -JavaScript/aula02.js
+++ Fundamentos -JavaScript/aula02.js
@@ -1,7 +1,11 @@
-const numero = 22;
+const numero = 21;
 
 const numeroPar = (numero % 2) == 0;
 
-console.log(numeroPar);
+if (numeroPar) { //se o resto do "numero", for igual a 0 é true
+    console.log('Par');
+} else {
+    console.log('Impar');
+}
 
 // resto da divisão
