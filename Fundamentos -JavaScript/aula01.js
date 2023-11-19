40d226533afba1ea71e5e4c02a2e60f8f52fe555
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-19 09:53:11 -0300

    Declaração de variáveis

--- Fundamentos -JavaScript/aula01.js
+++ Fundamentos -JavaScript/aula01.js
@@ -1,3 +1,12 @@
 
-console.log('Hello World')
+//duas formas de declarar variáveis
+
+let variavel = 10;  // dessa maneira é a maneira que se declara a variavel.
+
+variavel = 20; //aqui eu posso alterar o valor que tinha antes
+
+const pi = 3.14;  // conts é como se fosse o Final em java, uma vez declarada não pode substituir o valor.
+
+console.log(variavel)
+
 //Dessa forma é para imprimir
