fd395a06c527fee627ea98aeefdd8eb87c7249cf
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-23 10:50:05 -0300

    1º Exemplo de como utilizar objetos

---
+++ Fundamentos -JavaScript/objetos/aula01.js
@@ -0,0 +1,10 @@
+
+const junior = {
+    nome: 'Jorge W M Junior',
+    idade: 25
+};
+
+//Há 3 formas de imprimir esse objeto;
+console.log(junior);
+console.log(junior.nome);
+console.log(junior.idade);
