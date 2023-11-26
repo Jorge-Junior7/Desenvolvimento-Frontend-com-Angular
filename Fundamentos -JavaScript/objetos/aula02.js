063e42e2691d244aa65472225c478cf18070e3bb
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-26 16:08:16 -0300

    Aula02.2 Exercicio

--- Fundamentos -JavaScript/objetos/aula02.js
+++ Fundamentos -JavaScript/objetos/aula02.js
@@ -14,22 +14,17 @@ class Pessoa { //O inicio de toda a classe inicia com a letra maiúscula
     }
 }
 
-//É MAIS PRÁTICO DESSE MODO:
+function compararPessoas(p1, p2){
+    if (p1.idade > p2.idade){
+         console.log(p1.nome, ' é mais velho(a) quê ', p2.nome);
+    } else if(p1.idade < p2.idade){
+        console.log(p1.nome, ' é mais novo quê ', p2.nome);
+    } else {
+        console.log(p1.nome, ' e ', p2.nome, ' tem a mesma idade');
+    }
+}
 
-const junior = new Pessoa('Junin', 19);
-console.log(junior);
+const junior = new Pessoa('Junior', 19);
+const talha = new Pessoa('Talha', 12);
 
-
-
-//NO LUGAR DE FAZER ASSIM, sem o método constructor:
-
-/*
-const junior = new Pessoa();
-junior.nome = 'Jorge H Michael Junior';
-junior.idade = 19;
-
-
-console.log(junior);
-*/
-
-
+compararPessoas(junior, talha);
