7e980abfd67f4f7ea98a1c6853e948a0b3d472bc
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-26 10:16:46 -0300

    Aula 01.2 explicação parte 3

--- Fundamentos -JavaScript/objetos/aula01.js
+++ Fundamentos -JavaScript/objetos/aula01.js
@@ -1,15 +1,22 @@
-
-const junior = {
+const pessoa = {
     nome: 'Jorge W M Junior',
-    idade: 40
+    idade: 40,
+
+    descrever: function () {
+        console.log('Meu nome é ', this.nome, 'e minha idade é ', this.idade);
+    }
 };
 
-junior.altura = 1.70; 
-//observe chamei o objeto adicionei mais um parâmetro
+// Redefine a função descrever para exibir APENAS o nome
+pessoa.descrever = function () {
+    console.log('Nome é', this.nome);
+}
 
-delete junior.nome; 
-//Observe que aqui eu removi esse parâmetro
+// Altera o nome, idade, adiciona altura e deleta a propriedade altura da pessoa
+pessoa.nome = 'Juliana'; // Altera o nome para 'Juliana'
+pessoa.idade = 10; // Altera a idade para 10
+pessoa.altura = 1.70; // Adiciona a altura
+delete pessoa.altura; // Deleta a propriedade altura do objeto pessoa
 
-console.log(junior);
-
-
+pessoa.descrever(); 
+// Chama a função descrever para exibir somente o nome
