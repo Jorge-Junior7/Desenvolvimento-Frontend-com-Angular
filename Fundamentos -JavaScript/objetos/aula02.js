3ec111ad3a482e372033e510c86809bc09ba03fc
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-26 15:32:59 -0300

    Aula 02.1 Método Constructor

--- Fundamentos -JavaScript/objetos/aula02.js
+++ Fundamentos -JavaScript/objetos/aula02.js
@@ -1,22 +1,35 @@
-//criando classe e instânciando-as
+//criando classe, instânciando-as com o método CONSTRUCTOR
 
 class Pessoa { //O inicio de toda a classe inicia com a letra maiúscula
     nome;
     idade;
 
-    descrever(){
+    constructor(nome, idade) {
+      this.nome = nome;
+      this.idade = idade;
+    }
+
+    descrever() {
         console.log('O nome é ', this.nome, ' e idade ', this.idade);
     }
 }
 
+//É MAIS PRÁTICO DESSE MODO:
+
+const junior = new Pessoa('Junin', 19);
+console.log(junior);
+
+
+
+//NO LUGAR DE FAZER ASSIM, sem o método constructor:
+
+/*
 const junior = new Pessoa();
 junior.nome = 'Jorge H Michael Junior';
 junior.idade = 19;
 
-const talha = new Pessoa();
-talha.nome = 'Nathália Vitória da Silva Nascimento';
-talha.idade = 12;
 
 console.log(junior);
-console.log(talha);
+*/
+
 
