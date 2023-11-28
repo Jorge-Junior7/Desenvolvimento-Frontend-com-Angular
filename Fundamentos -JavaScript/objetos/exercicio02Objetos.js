b9e005968b2ea7d114dddd97c1b08b154686c028
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-27 21:20:48 -0300

    organizando nome de pastas/Exercicio (POO)

---
+++ Fundamentos -JavaScript/objetos/exercicio02Objetos.js
@@ -0,0 +1,40 @@
+/*
+  2) Crie uma classe para representar pessoas. Para cada pessoa teremos os atributos os atributos nome, peso e altura. As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura)); 
+  Instancie uma pessoa camada José que tenha 70kg de peso e 1,75 de altura ao José para o valor do seu IMC;
+
+*/
+
+class Pessoa {
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura) {
+     this.nome = nome;
+     this.peso = peso;
+     this.altura = altura;
+    }
+
+    calcularImc() {
+       return this.peso / (this.altura * this.altura);
+        
+    }
+
+       classificarImc() {
+        const imc = this.calcularImc();
+
+        if (imc < 18.5) {
+            return ('Abaixo do peso');
+        } else if (imc >= 18.5 && imc < 25) {
+            return ('Peso normal');
+        } else if (imc >= 25 && imc <= 30) {
+            return ('Acima do peso');
+        } else {
+            return ('Obesidade Grave');
+        }
+    }
+}
+
+const p1 = new Pessoa('José', 70, 1.75);
+console.log(p1.classificarImc());
+
