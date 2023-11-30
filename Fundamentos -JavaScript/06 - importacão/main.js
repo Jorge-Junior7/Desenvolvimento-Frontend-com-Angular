ac22a19d9b638d63cfe83addfc1ed807194620f4
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-11-30 10:43:42 -0300

    Simplificando o cód

--- Fundamentos -JavaScript/06 - importacão/main.js
+++ Fundamentos -JavaScript/06 - importacão/main.js
@@ -12,23 +12,17 @@
     98
 */
 
-const { gets, print } = require('./funcoes-auxiliares'); // Importa as funções gets e print do arquivo 'funcoes-auxiliares'
+const { gets, print } = require('./funcoes-auxiliares'); // 
 
-const listaNumerosSorteados = []; // Inicializa um array vazio para armazenar os números sorteados
+const quantidadeDeAlunos = gets();
+let maiorValorEncontrado = 0;
 
-for (let i = 0; i < 5; i++) {
-   const numeroSorteado = gets(); // Chama a função gets para obter um número sorteado
-   listaNumerosSorteados.push(numeroSorteado); // Adiciona o número sorteado à lista de números sorteados
-}
-
-let maiorNumero = 0; // Inicializa a variável que armazenará o maior número encontrado
-
-for (let i = 0; i < listaNumerosSorteados.length; i++){
-    const numeroAtual = listaNumerosSorteados[i]; // Obtém o número atual da lista de números sorteados
-    if (numeroAtual > maiorNumero){ // Verifica se o número atual é maior que o maior número encontrado até agora
-        maiorNumero = numeroAtual; // Se o maiorNumero for maior que o numeroAtual ele atribui o valor do maior para o numero atual
+for (let i = 0; i < quantidadeDeAlunos; i++){
+    const numerosSorteados = gets();
+    if(numerosSorteados > maiorValorEncontrado){
+         maiorValorEncontrado = numerosSorteados;
     }
 }
 
-print(listaNumerosSorteados); // Imprime a lista completa dos números sorteados
-print(maiorNumero); // Imprime o maior número sorteado entre os alunos
+print(quantidadeDeAlunos);
+print(maiorValorEncontrado);
