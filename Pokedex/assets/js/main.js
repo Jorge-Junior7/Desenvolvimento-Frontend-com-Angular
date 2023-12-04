11351305fa0cd60e738465232b585949c982010b
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-12-03 21:51:03 -0300

    Adicionando requisição com js

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('Sucesso');
+const offset = 0; //define a página
+const limit = 10; //define a quantidade de itens
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`; //
+
+fetch(url)
+   //Versão com arrow
+   .then((response) => response.json())//converte a promess: json
+   .then((jsonBody) => console.log(jsonBody)) //imprime a api
+   .catch((error) => console.error(error))
+   //sempre mlhr  utilizar essa maneira de erro pois exibe os detalhes do erro
+
+
+
