32a461af7ab0364a2e904589066d23048bddc1ec
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-12-05 11:09:53 -0300

    Transformando a lista de Pokémon em uma lista de HTML

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,13 +1,32 @@
-const offset = 0; //define a página
-const limit = 10; //define a quantidade de itens
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`; //
+const offset = 0; // define a página
+const limit = 10; // define a quantidade de itens
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function convertPokemonToLi(pokemon) {
+  return `<li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+      <ol class="types">
+        <li class="type">grass</li>
+        <li class="type">poison</li>
+      </ol>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+        alt="${pokemon.name}">
+    </div>
+  </li>`;
+}
+
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
-   //Versão com arrow
-   .then((response) => response.json())//converte a promess: json
-   .then((jsonBody) => console.log(jsonBody)) //imprime a api
-   .catch((error) => console.error(error))
-   //sempre mlhr  utilizar essa maneira de erro pois exibe os detalhes do erro
-
-
-
+  .then((response) => response.json()) // converte a resposta para JSON
+  .then((jsonBody) => jsonBody.results) // obtém a lista de pokemons
+  .then((pokemons) => { // manipula a lista de pokemons
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokemonList.innerHTML += convertPokemonToLi(pokemon);
+      // converte cada pokemon para HTML e o adiciona à lista de pokemons
+    }
+  })
+  .catch((error) => console.error(error)); // lida com erros, se houverem
