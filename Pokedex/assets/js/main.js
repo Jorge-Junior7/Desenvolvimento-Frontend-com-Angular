6e25f1b39696a1775b93731c009d7303db91bc26
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2023-12-06 21:49:00 -0300

    Manipulando múltiplas requisições em paralelo

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,20 +1,22 @@
-const offset = 0; // define a página
-const limit = 10; // define a quantidade de itens
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon) {
-  return `<li class="pokemon">
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-      <ol class="types">
-        <li class="type">grass</li>
-        <li class="type">poison</li>
-      </ol>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="${pokemon.name}">
-    </div>
-  </li>`;
+  return `
+      <li class="pokemon">
+         <span class="number">${pokemon.order}</span>
+         <span class="name">${pokemon.name}</span>
+
+         <div class="detail">
+         <ol class="types">
+         ${convertPokemonTypesToLi(pokemon.types).join('')}
+         </ol>
+         <img src="${pokemon.sprites.other.dream_world.front_default}"
+         alt="${pokemon.name}">
+         </div>
+      </li>
+  `;
 }
 
 const pokemonList = document.getElementById('pokemonList');
