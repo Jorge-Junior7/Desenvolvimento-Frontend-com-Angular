e87237ddcf66936a7b1429c91455d0046e31e032
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2024-01-09 15:37:38 -0300

    Criando a estrutura

--- Componentes-Fundamentos/src/Componentes/CardNews.js
+++ Componentes-Fundamentos/src/Componentes/CardNews.js
@@ -2,36 +2,39 @@ class Cardnews extends HTMLElement {
     constructor() {
         super();
 
-        // Criando o Shadow DOM para o componente
         const shadow = this.attachShadow({ mode: "open" });
 
-        // Chamando a função 'build()' para criar a estrutura do componente e adicioná-la ao Shadow DOM
         shadow.appendChild(this.build());
 
-        // Chamando a função 'styles()' para adicionar os estilos ao Shadow DOM
         shadow.appendChild(this.styles());
     }
 
- // Função para construir a estrutura do componente
-build() {
-    // Criando o elemento principal do componente, um <div> com a classe "card"
-    const componentRoot = document.createElement("div");
-    componentRoot.setAttribute("class", "card");
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
-    // Criando elementos para a parte esquerda e direita do cartão, ambos filhos do elemento "componentRoot"
-    const cardLeft = document.createElement("div");
-    cardLeft.setAttribute("class", "card__left");
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
 
-    const cardRight = document.createElement("div");
-    cardRight.setAttribute("class", "card__right");
+        const autor = document.createElement("span");
+        const linkTitle = document.createElement("a");
+        const newsContent = document.createElement("p");
 
-    // Adicionando os elementos "cardLeft" e "cardRight" como filhos do elemento "componentRoot"
-    componentRoot.appendChild(cardLeft);
-    componentRoot.appendChild(cardRight);
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
 
-    // Retornando a estrutura do componente montada, que contém os elementos "cardLeft" e "cardRight" como partes do "componentRoot"
-    return componentRoot;
-}
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        cardRight.appendChild(newsImage)
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
 
     styles() {
 
