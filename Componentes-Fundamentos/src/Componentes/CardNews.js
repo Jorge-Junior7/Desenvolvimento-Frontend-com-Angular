cce5b84b22d3135e80f570ec0eae5b7e15b875cb
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2024-01-09 14:54:42 -0300

    Encapsulando

--- Componentes-Fundamentos/src/Componentes/CardNews.js
+++ Componentes-Fundamentos/src/Componentes/CardNews.js
@@ -1,16 +1,41 @@
-// Define uma nova classe chamada CardNews que estende a funcionalidade do HTMLElement
-class CardNews extends HTMLElement {
-    constructor(){
-        // Chama o construtor da classe pai (HTMLElement)
+class Cardnews extends HTMLElement {
+    constructor() {
         super();
 
-        // Cria um Shadow DOM dentro do elemento customizado 'card-news'
-        const shadow = this.attachShadow({mode: "open"}); // Define o modo como 'open', permitindo acesso externo ao Shadow DOM
+        // Criando o Shadow DOM para o componente
+        const shadow = this.attachShadow({ mode: "open" });
 
-        // Define o conteúdo dentro do Shadow DOM
-        shadow.innerHTML = "<h1>Hello World</h1>"; // Insere um cabeçalho h1 com o texto 'Hello World'
+        // Chamando a função 'build()' para criar a estrutura do componente e adicioná-la ao Shadow DOM
+        shadow.appendChild(this.build());
+
+        // Chamando a função 'styles()' para adicionar os estilos ao Shadow DOM
+        shadow.appendChild(this.styles());
+    }
+
+ // Função para construir a estrutura do componente
+build() {
+    // Criando o elemento principal do componente, um <div> com a classe "card"
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
+
+    // Criando elementos para a parte esquerda e direita do cartão, ambos filhos do elemento "componentRoot"
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class", "card__left");
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card__right");
+
+    // Adicionando os elementos "cardLeft" e "cardRight" como filhos do elemento "componentRoot"
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+
+    // Retornando a estrutura do componente montada, que contém os elementos "cardLeft" e "cardRight" como partes do "componentRoot"
+    return componentRoot;
+}
+
+    styles() {
+
     }
 }
 
-// Registra o novo elemento customizado 'card-news' com o nome da classe 'CardNews'
-customElements.define('card-news', CardNews);
+customElements.define("card-news", Cardnews);
