6d10c3b7da42eac35042a45f229e67b97e6c9050
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2024-02-06 21:42:24 -0300

    Concluído com sucesso, compação de contas bancarias

--- Desafios01/02.js
+++ Desafios01/02.js
@@ -32,12 +32,14 @@ Pedro
 Pedro
 1252
 400.0	As contas bancárias são diferentes.
+
 Ju
 01090
 80.0
 Ju
 01090
 80.0	As contas bancárias são iguais.
+
 Tadeu
 11111
 600.0
@@ -51,21 +53,24 @@ João
 //- "print": imprime um texto de saída (output), pulando linha.
 
 // TODO: Crie o objeto JSON do Cliente 1 e suas propriedades (conta e saldo):
-const   = { 
-    nome: gets(), 
-   
-  };
-  
-  // TODO: Crie o objeto JSON do Cliente 2 e suas propriedades (nome, conta e saldo):
-  const = { 
-    
+const cliente1 = {
+  nome: gets(), 
+  conta: gets(),
+  saldo: gets(),
   
   };
-  
-  //TODO: Compare os JSONs (nome, conta, saldo) dos Clientes, verificando se todas as propriedades são iguais:
-  
-  if ((cliente1.nome === cliente2.nome) && (cliente1.conta === ) && ( )){
-   print("As contas bancárias são iguais.");
-  } else {
-    print("As contas bancárias são diferentes.")
-  }
+
+// TODO: Crie o objeto JSON do Cliente 2 e suas propriedades (nome, conta e saldo):
+const cliente2 = {
+  nome: gets(),
+  conta: gets(),
+  saldo: gets(),
+  };
+
+//TODO: Compare os JSONs (nome, conta, saldo) dos Clientes, verificando se todas as propriedades são iguais:
+
+if ((cliente1.nome === cliente2.nome) && (cliente1.conta === cliente2.conta) && (cliente1.saldo === cliente2.saldo)) {
+  console.log("As contas bancárias são iguais.");
+} else {
+  console.log("As contas bancárias são diferentes.")
+}
