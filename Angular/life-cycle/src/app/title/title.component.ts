96a4e914f77f9cda4e93e2d964e9ee47de809971
Jorge-Junior7 <jorgejuniorvencedor@gmail.com>
2024-02-16 09:42:56 -0300

    OnInit

--- Angular/life-cycle/src/app/title/title.component.ts
+++ Angular/life-cycle/src/app/title/title.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleComponent implements OnInit {
 
+  nome: string = 'Junin'
   constructor() { }
 
   ngOnInit(): void {
+
+    this.nome= `ola ${this.nome}`
+
   }
+
 
 }
